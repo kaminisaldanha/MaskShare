7a9e2f24a19157d1b4c7daa9968516e7b38cd3b9
Yash Lekhwani <yashlekhwani@gmail.com>
2020-05-24 00:27:17 -0500

    fixed fields for migrations

--- maskshare/needer/models.py
+++ maskshare/needer/models.py
@@ -9,6 +9,6 @@ class Needer(models.Model):
     age = models.CharField(max_length=3)
     immunocompromised = models.BooleanField(default=False)
     mask_collection_preference = models.CharField(max_length=10)
-    longitude = models.DecimalField(maxdigits=18, decimal_places=15)
-    latitude = models.DecimalField(maxdigits=18, decimal_places=15)
+    longitude = models.DecimalField(max_digits=18, decimal_places=15)
+    latitude = models.DecimalField(max_digits=18, decimal_places=15)
     profile_creation = models.DateTimeField(auto_now_add=True)
